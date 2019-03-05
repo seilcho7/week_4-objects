93099da211e409f117c50deb35ec5bc42ef45bf6
Seil Cho <seilcho7@hotmail.com>
2019-03-05 10:55:31 -0500

    adds translation function

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 console.log('hello');
+
+// Keys are strings
+const leetDictionary = {
+    "A": 4,
+    "E": 3,
+    "G": 6,
+    "I": 1,
+    "O": 0,
+    "S": 5,
+    "T": 7,
+};
+
+function transl33t(originalString) {
+    let results = '';
+
+    return results;
+}
